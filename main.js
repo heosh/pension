7be9616333ebdb36db54f17a968b11473193bf59
heosh <81971258+heosh@users.noreply.github.com>
2021-06-09 21:56:37 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const myConnection = require("./db/db_conn");
+
+const express = require("express"),
+  app = express(),
+  layouts = require("express-ejs-layouts"),
+  router = require('./routes/index'),
+  mysql = require("mysql"),
+  session = require("express-session"),
+  MySQLStore = require("express-mysql-session")(session),
+  methodOverride = require("method-override");
+
+app.set("port", process.env.PORT || 3000);
+
+app.set("view engine", "ejs");
+app.use(express.static( __dirname+'/public'));
+
+//app.set("views", __dirname + '/views');
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]
+  })
+);
+
+
+app.set("layout", "layout")
+app.set("layout extractScripts", true);
+app.use(layouts);
+
+//mysql
+var sessionStore = new MySQLStore(myConnection.options);
+app.use(
+  session({
+    key: "testkey",
+    secret: "secret code",
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+var connection = myConnection.connection();
+myConnection.init(connection);
+var sessionStore = new MySQLStore({}, connection);
+
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`request made to: ${req.url}`);
+  next();
+});
+
+app.use("/", router);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
