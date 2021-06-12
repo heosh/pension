e06f15377120bc2f3f9587f53630ddcb86d1e918
JoHeeGun <83778431+JoHeeGun@users.noreply.github.com>
2021-06-12 17:39:28 +0900

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@
 const router = require("express").Router(),
   roomRoutes = require("./roomRoutes"),
   bookingRoutes = require("./bookingRoutes"),
+  loginRoutes = require("./loginRoutes"),
+  boardRoutes = require("./boardRoutes"),
   errorRoutes = require("./errorRoutes").get;
 
 
 router.get("/", (req, res) => {
   console.log("get in");
-  res.render('index');
+  console.log("현재 userId:",req.session.userId);
+  res.render('index',{userId : req.session.userId});
 });
 
 router.post("/", (req, res) => {
@@ -19,6 +22,8 @@ router.post("/", (req, res) => {
 
 router.use("/room", roomRoutes);
 router.use("/booking", bookingRoutes);
+router.use("/login",loginRoutes);
+router.use("/board",boardRoutes);
 router.use("/", errorRoutes);
 
 
